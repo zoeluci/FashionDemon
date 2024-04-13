cbeb172d9e27aa0fe9c1c719344ae556c6f2ee00
zoeluci <zoeluci.123@gmail.com>
2024-04-13 15:22:09 -0300

    Add colliders and basic character movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float speed;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal")* speed, body.velocity.y);
+    }
+}
