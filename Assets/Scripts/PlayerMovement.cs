a82a3bd01cb9ba418c53416fe07333bd17eea488
zoeluci <zoeluci.123@gmail.com>
2024-04-13 16:34:13 -0300

    Update jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
-    public float RunSpeed;
-    public float JumpSpeed;
+    Rigidbody2D body;
+    public float runSpeed;
+    public float jumpSpeed;
+    bool grounded;
 
-    private void Awake()
+    void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")* RunSpeed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * runSpeed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, JumpSpeed);
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.W) && grounded)
+            Jump();
     }
+
+    void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
 }
