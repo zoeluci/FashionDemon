054fc639daa7f28e3f627c86671f14e87096c3ab
zoeluci <zoeluci.123@gmail.com>
2024-04-14 02:26:30 -0300

    Update environment

--- Assets/Scripts/PlayerObstacle.cs
+++ Assets/Scripts/PlayerObstacle.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacles : MonoBehaviour
+public class PlayerObstacle : MonoBehaviour
 {
-    bool isDamaged = false;
     public GameObject gameOverScreen;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("Player"))
-            isDamaged = true;
-
-        if (isDamaged)
+        if (collision.gameObject.tag.Equals("Obstacle"))
         {
             gameOverScreen.SetActive(true);
-            gameObject.SetActive(false);
+            Time.timeScale = 0;
         }
     }
 }
