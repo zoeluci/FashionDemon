1135f5f1dd8363c44d57e9b8766a1146ee7a51db
zoeluci <zoeluci.123@gmail.com>
2024-04-14 02:27:46 -0300

    Update ClothesItem script

--- Assets/Scripts/ClothesItem.cs
+++ Assets/Scripts/ClothesItem.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class ClothesItem : MonoBehaviour
 {
-    bool isCollected = false;
     public GameObject clothesIcon;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
-            isCollected = true;
-
-        if (isCollected)
         {
             clothesIcon.SetActive(true);
             Destroy(gameObject);
