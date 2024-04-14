b58a3e0f10aae78e355d1cf70df8a3e124a88e5b
zoeluci <zoeluci.123@gmail.com>
2024-04-14 02:42:22 -0300

    Update PlayerObstacle

--- Assets/Scripts/PlayerObstacle.cs
+++ Assets/Scripts/PlayerObstacle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerObstacle : MonoBehaviour
 {
     public GameObject gameOverScreen;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Obstacle"))
